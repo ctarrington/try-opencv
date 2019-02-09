f95d43ef0a3927583a015492a63cc5e1b54bd238
CT Arrington <arrington.ct@gmail.com>
2019-02-09 17:20:29 -0500

    drawing on video

--- src/simple-opencv/video.cpp
+++ src/simple-opencv/video.cpp
@@ -7,19 +7,29 @@ using namespace cv;
 
 int main(int argc, char** argv)
 {
+    const int thickness = 3;
     VideoCapture cap;
     cap.open(0);
     if(!cap.isOpened()) {
        return -1;
     }
 
-    namedWindow("Video",1);
+    namedWindow("Color Video", WINDOW_AUTOSIZE);
+    namedWindow("Grayscale Video", WINDOW_AUTOSIZE);
+
     for(;;) {
         Mat frame;
+        Mat grayscale;
+
         cap >> frame;
 
-        imshow("Video", frame);
+        line(frame, Point(0,0), Point(frame.cols, frame.rows), Scalar(255,0,0), thickness);
+        line(frame, Point(0,frame.rows), Point(frame.cols, 0), Scalar(255,0,0), thickness);
 
+        cvtColor(frame, grayscale, COLOR_RGB2GRAY);
+
+        imshow("Color Video", frame);
+        imshow("Grayscale Video", grayscale);
 
         if(waitKey(30) >= 0) {
             break;
@@ -27,6 +37,7 @@ int main(int argc, char** argv)
 
     }
 
+    cap.release();
     return EXIT_SUCCESS;
 }
 
