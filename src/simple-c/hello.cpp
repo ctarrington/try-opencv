df89d1a8ae55f837cbb319e00d772a3b051e3705
ctarrington <arrington.ct@gmail.com>
2019-02-08 22:14:27 -0500

    experimenting with c++ project structure

---
+++ src/simple-c/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "greet.h"
+
+using namespace std;
+
+int main() {
+   cout << "yo world " << formal("Fred") << endl;
+   return 0;
+}
---
+++ src/simple-c/utils/greet.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+#include "greet.h"
+
+
+string formal(string name) {
+   return "Hello there, " + name;
+}
